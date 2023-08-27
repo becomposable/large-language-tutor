7710a7004e074cd75fc939fa33bdf799fea5a926
Eric Barroca <eric@vintners.co>
2023-08-28 04:16:45 +0900

    testing

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,4 +1,4 @@
-const SERVER_BASE_URL = process.env.SERVER_BASE_URL || "http://localhost:8089";
+const SERVER_BASE_URL = process.env.SERVER_BASE_URL || "https://lang-tutor-jc6rsiqkva-uc.a.run.app";
 const API_BASE_URL = `${SERVER_BASE_URL}/api/v1`;
 
 export { API_BASE_URL }
