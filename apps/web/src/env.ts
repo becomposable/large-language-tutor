a19620b41f8fe97b5be84271a553c5a9d7c3e12d
Eric Barroca <eric@vintners.co>
2023-08-28 04:32:27 +0900

    testing

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,4 +1,4 @@
 const SERVER_BASE_URL = process.env.SERVER_BASE_URL || "https://lang-tutor-jc6rsiqkva-uc.a.run.app";
-const API_BASE_URL = `${SERVER_BASE_URL}/api/v1`;
+const API_BASE_URL = `${SERVER_BASE_URL}/api/v2`;
 
 export { API_BASE_URL }
