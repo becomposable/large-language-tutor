07719986abbc5d9f9ab65a1654111bbdb234f055
Eric Barroca <eric@vintners.co>
2023-08-28 03:34:47 +0900

    set server URL

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -1,3 +1,4 @@
-const API_BASE_URL = "http://localhost:8089/api/v1";
+const SERVER_BASE_URL = process.env.SERVER_BASE_URL || "http://localhost:8089";
+const API_BASE_URL = `${SERVER_BASE_URL}/api/v1`;
 
 export { API_BASE_URL }
