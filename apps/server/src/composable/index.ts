105f18c898e949000ff222b48a0ffdc701649273
Eric Barroca <eric@vintners.co>
2023-10-31 03:45:44 +0900

    throw if api key missing

--- apps/server/src/composable/index.ts
+++ apps/server/src/composable/index.ts
@@ -1,5 +1,11 @@
 import { configure } from "./interactions.js";
 
+const apiKey = process.env.COMPOSABLE_PROMPT_API_KEY;
+
+if (!apiKey) {
+  throw new Error("COMPOSABLE_PROMPT_API_KEY environment variable must be set");
+}
+
 configure({
-  apikey: process.env.COMPOSABLE_PROMPT_API_KEY,
+  apikey: apiKey,
 });
